9386ca08dfb5a04f790381882f44042eb791d6a1
hx210 <76199340+hx210@users.noreply.github.com>
2021-11-25 23:47:44 +0800

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,5 @@
+# UPX 可执行软件压缩
+sed -i '/patchelf pkgconf/i\tools-y += ucl upx' ./tools/Makefile
+sed -i '\/autoconf\/compile :=/i\$(curdir)/upx/compile := $(curdir)/ucl/compile' ./tools/Makefile
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/tools/upx tools/upx
+svn co https://github.com/immortalwrt/immortalwrt/branches/master/tools/ucl tools/ucl
